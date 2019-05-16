d1ec178f7c8d8d327ee418fad64759660ed63f73
Pouya <mail.pouyaparsa@gmail.com>
2019-05-16 20:04:31 +0430

    adding navigation drawer

--- app/src/main/java/com/pouya/digim/MainActivity.java
+++ app/src/main/java/com/pouya/digim/MainActivity.java
@@ -3,9 +3,18 @@ package com.pouya.digim;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,11 +25,14 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter recyclerViewAdapter;
     private List<Product> Products;
+
+    private String category = "laptops";
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,15 +48,61 @@ public class MainActivity extends AppCompatActivity {
 
         Products = new ArrayList<>();
 
+        this.loadProducts(this.category);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.draw_layout);
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(this,
+                        drawer,
+                        toolbar,
+                        R.string.navigation_drawer_open,
+                        R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+//    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Log.d("eoo", "hello");
+        switch (menuItem.getItemId()) {
+            case R.id.laptops :
+                Toast.makeText(getApplicationContext(), "laptops selected", Toast.LENGTH_SHORT).show();
+                this.loadProducts("laptops");
+                break;
+            case R.id.shirts:
+                this.loadProducts("shirts");
+                break;
+            case R.id.fruits:
+                this.loadProducts("fruits");
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    public void loadProducts(String category) {
+        Products.clear();
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference dbRef = firebaseDatabase.getReference("shirts/");
+        DatabaseReference dbRef = firebaseDatabase.getReference(category + "/");
 
         dbRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
 
-                for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren()) {
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
 
                     Product product = dataSnapshot1.getValue(Product.class);
                     Products.add(product);
@@ -57,7 +115,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     public void setAdapterWithData() {
